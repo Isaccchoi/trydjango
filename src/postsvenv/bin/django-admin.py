5ff21df1abf92d4a6bc37265bc6f89ebbc234ea6
isaccchoi <isaccchoi@naver.com>
2016-12-13 00:21:08 +0900

    second commit

---
+++ src/postsvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/choijihyun/Desktop/Python/Udemy/try-django-19/src/postsvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
